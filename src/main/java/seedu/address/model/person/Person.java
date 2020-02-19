f3750dcf5beaf2448ca2ab507b41f1dc20f23a92
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-02-20 02:48:35 +0800

    Update Storage, UI, and RemarkCommand#execute() tests

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -29,12 +29,12 @@ public class Person {
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
-        this.remark = remark;
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, remark, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.remark = remark;
         this.tags.addAll(tags);
     }
 
